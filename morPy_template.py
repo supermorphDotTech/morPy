b62b404835301174f714bc2011776921bd70a13d
supermorph.tech <148707180+supermorphDotTech@users.noreply.github.com>
2025-01-02 13:18:28 +0100

    Most recent dev

--- morPy_template.py
+++ morPy_template.py
@@ -1,71 +1,186 @@
-"""
+r"""
 morPy Framework by supermorph.tech
 https://github.com/supermorphDotTech
 
 Author:     AUTHOR
 Descr.:     DESCRIPTION
 
-Annotations for Spyder:
-    TODO
-    FIXME
-    XXX
-    HINT
-    TIP
-    @todo
-    HACK
-    BUG
-    OPTIMIZE
-    !!!
-    ???
+Annotations:
+    #TODO
+    #FIXME
 """
 
 import mpy
 import sys
-    
-from mpy_decorators import metrics
+
+from mpy_decorators import metrics, log
 
 @metrics
-def template(
-    mpy_trace: str,
-    prj_dict: dict
-) -> dict:
+def template(mpy_trace: dict, app_dict: dict) -> dict:
 
-    """ 
-    This function does ???
-    
+    r"""
+    This function is a template.
+
     :param mpy_trace: operation credentials and tracing information
-    :param prj_dict: morPy global dictionary containing project configurations
+    :param app_dict: morPy global dictionary containing app configurations
 
     :return: dict
+        mpy_trace: Operation credentials and tracing
         check: Indicates whether the function ended without errors
-    
+
     :example:
-        template(mpy_trace, prj_dict)
+        template(mpy_trace, app_dict)
     """
 
     # morPy credentials (see mpy_init.init_cred() for all dict keys)
-    module = '???'
-    operation = '???(~)'
+    module = 'morPy_templates'
+    operation = 'template(~)'
     mpy_trace = mpy.tracing(module, operation, mpy_trace)
 
     # OPTION enable/disable logging
     # ??? mpy_trace["log_enable"] = False
 
-    # Operation preparation
-    check = False 
+    check = False
 
     try:
+        # LOCALIZED_MESSAGE
+        log(mpy_trace, app_dict, "debug",
+        lambda: f'{app_dict["loc"]["app"]["LOCALIZED_MESSAGE"]}')
+
         # TODO: MY CODE
+
         check = True
 
     except Exception as e:
-        log_message = (
-            f'{prj_dict["loc"]["mpy"]["err_line"]}: {sys.exc_info()[-1].tb_lineno}\n'
-            f'{prj_dict["loc"]["mpy"]["err_excp"]}: {e}'
-        )
-        mpy.log(mpy_trace, prj_dict, log_message, 'error')
+        log(mpy_trace, app_dict, "error",
+        lambda: f'{app_dict["loc"]["mpy"]["err_line"]}: {sys.exc_info()[-1].tb_lineno}\n'
+                f'{app_dict["loc"]["mpy"]["err_excp"]}: {e}')
 
     return{
         'mpy_trace' : mpy_trace,
         'check' : check,
         }
+
+class cl_template:
+    r"""
+    This class is a template.
+    """
+
+    def __init__(self, mpy_trace: dict, app_dict: dict) -> None:
+        r"""
+        In order to get metrics for __init__(), call helper method _init() for
+        the @metrics decorator to work. It relies on the returned
+        {'mpy_trace' : mpy_trace}, which __init__() can not do (needs to be None).
+
+        :param mpy_trace: operation credentials and tracing information
+        :param app_dict: morPy global dictionary containing app configurations
+
+        :return:
+            -
+
+        :example:
+            instance = cl_template(mpy_trace, app_dict)
+        """
+
+        # morPy credentials (see mpy_init.init_cred() for all dict keys)
+        module = 'morPy_templates'
+        operation = 'cl_template.__init__(~)'
+        mpy_trace = mpy.tracing(module, operation, mpy_trace)
+
+        # OPTION enable/disable logging
+        # ??? mpy_trace["log_enable"] = False
+
+        try:
+            # Use self._init() for initialization
+            self._init(mpy_trace, app_dict)
+
+        except Exception as e:
+            log(mpy_trace, app_dict, "error",
+            lambda: f'{app_dict["loc"]["mpy"]["err_line"]}: {sys.exc_info()[-1].tb_lineno}\n'
+                    f'{app_dict["loc"]["mpy"]["err_excp"]}: {e}')
+
+    @metrics
+    def _init(self, mpy_trace: dict, app_dict: dict) -> dict:
+        r"""
+        Helper method for initialization to ensure @metrics decorator usage.
+
+        :param mpy_trace: operation credentials and tracing information
+        :param app_dict: morPy global dictionary containing app configurations
+
+        :return: dict
+            mpy_trace: Operation credentials and tracing
+            check: Indicates whether the function ended without errors
+
+        :example:
+            self._init(mpy_trace, app_dict)
+        """
+
+        # morPy credentials (see mpy_init.init_cred() for all dict keys)
+        module = 'morPy_templates'
+        operation = 'cl_template._init(~)'
+        mpy_trace = mpy.tracing(module, operation, mpy_trace)
+
+        # OPTION enable/disable logging
+        # ??? mpy_trace["log_enable"] = False
+
+        check = False
+
+        try:
+            # TODO: MY INSTANCE INITIALIZATION
+            # LOCALIZED_MESSAGE
+            log(mpy_trace, app_dict, "debug",
+            lambda: f'{app_dict["loc"]["app"]["LOCALIZED_MESSAGE"]}')
+
+            check = True
+
+        except Exception as e:
+            log(mpy_trace, app_dict, "error",
+            lambda: f'{app_dict["loc"]["mpy"]["err_line"]}: {sys.exc_info()[-1].tb_lineno}\n'
+                    f'{app_dict["loc"]["mpy"]["err_excp"]}: {e}')
+
+        return{
+            'mpy_trace' : mpy_trace,
+            'check' : check,
+            }
+
+    @metrics
+    def method(self, mpy_trace: dict, app_dict: dict) -> dict:
+        r"""
+        :param mpy_trace: operation credentials and tracing information
+        :param app_dict: morPy global dictionary containing app configurations
+
+        :return: dict
+            mpy_trace: Operation credentials and tracing
+            check: Indicates whether the function ended without errors
+
+        :example:
+            self.method(mpy_trace, app_dict)
+        """
+
+        # morPy credentials (see mpy_init.init_cred() for all dict keys)
+        module = 'morPy_templates'
+        operation = 'cl_template.method(~)'
+        mpy_trace = mpy.tracing(module, operation, mpy_trace)
+
+        # OPTION enable/disable logging
+        # ??? mpy_trace["log_enable"] = False
+
+        check = False
+
+        try:
+            # TODO: MY CLASS METHOD
+            # LOCALIZED_MESSAGE
+            log(mpy_trace, app_dict, "debug",
+            lambda: f'{app_dict["loc"]["app"]["LOCALIZED_MESSAGE"]}')
+
+            check = True
+
+        except Exception as e:
+            log(mpy_trace, app_dict, "error",
+            lambda: f'{app_dict["loc"]["mpy"]["err_line"]}: {sys.exc_info()[-1].tb_lineno}\n'
+                    f'{app_dict["loc"]["mpy"]["err_excp"]}: {e}')
+
+        return{
+            'mpy_trace' : mpy_trace,
+            'check' : check,
+            }
