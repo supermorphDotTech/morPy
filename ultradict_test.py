a5f29b7b88d1e8e13fcc69730e33eb63b3164858
supermorph.tech <148707180+supermorphDotTech@users.noreply.github.com>
2025-01-03 12:32:46 +0100

    Update ultradict_test.py
    Additional debug statements (commented)

--- ultradict_test.py
+++ ultradict_test.py
@@ -1,3 +1,5 @@
+import sys
+
 from multiprocessing import Process, current_process, active_children
 from functools import partial
 from math import sqrt
@@ -30,9 +32,7 @@ def parallel_task(app_dict):
         process = current_process()
 
         i = 0
-        app_dict["test_count"] = i
-        app_dict["nested_udict"]["nested_dict"] = {}
-        total = 10
+        total = 10**2
         tmp_val = 0
 
         # Hold on until all processes are ready
@@ -42,15 +42,32 @@ def parallel_task(app_dict):
         while i < total:
             i += 1
             # Read and write app_dict and nested dictionaries
-            app_dict["test_count"] += 1
-            app_dict["nested_udict"]["nested_dict"].update({f'{i}' : process.name})
-            if i != app_dict["test_count"]:
-                raise RuntimeError("Counter mismatch!")
+            with app_dict.lock:
+                app_dict["test_count"] += 1
+                # app_dict["nested_udict"].update({f'{app_dict["test_count"]}' : process.name})
+
+                #### DEBUG BLOCK BELOW ####
+                # print(f'BEFORE: {process.name} => {app_dict["nested_udict"].name} => {app_dict}')
+                # app_dict.print_status()
+                # app_dict['nested_udict'].print_status()
+                # app_dict["test_count"] += 1
+                #### DEBUG BLOCK ABOVE ####
+
+                with app_dict["nested_udict"].lock:
+                    app_dict["nested_udict"][app_dict["test_count"]] = process.name
+
+                #### DEBUG BLOCK BELOW ####
+                # print(f'AFTER: {process.name} => {app_dict["nested_udict"].name} => {app_dict}')
+                # app_dict.print_status()
+                # app_dict['nested_udict'].print_status()
+                #### DEBUG BLOCK ABOVE ####
+
+            print(f'{app_dict["test_count"]} :: {process.name}')
             while tmp_val < total:
                 tmp_val = (sqrt(sqrt(i)*i) / i) + tmp_val**2
 
     except Exception as e:
-        print(f'ERROR: {e}')
+        print(f'Line: {sys.exc_info()[-1].tb_lineno}\n{e}\n')
 
 def spawn_wrapper(task):
     """ Wrapper for the task tuple / wrapped by the spawned process"""
@@ -67,21 +84,24 @@ if __name__ == '__main__':
     # If True, workers will start executing
     app_dict["run"] = False
 
+    # Some value in shared memory
+    app_dict["test_count"] = 0
+
     task = [parallel_task, app_dict]
 
     i = 0
     j = 10 # Processes to be started
+    processes = []
     while i < j:
         p = Process(target=partial(spawn_wrapper, task))
         p.start()
+        processes.append(p)
         i += 1
 
     # Release workers for calculations
     app_dict["run"] = True
 
     print(f'\nJoining processes.\n')
-    p.join()
-
-    # Print the calculations in relation to the executing process.
-    for step, proc in app_dict["nested_udict"]["nested_dict"]:
-        print(f'{step} :: {proc}')
+    # Now join everyone
+    for p in processes:
+        p.join()
